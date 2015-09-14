c71d4daf353b18e9118425cf33ca39b7895412de
Csaba J Santa <santacs@gmail.com>
2015-09-14 21:26:08 +0200

    Add first test skeleton

---
+++ src/test/java/org/tdd/auctionsniper/support/FakeAuctionServer.java
@@ -0,0 +1,26 @@
+package org.tdd.auctionsniper.support;
+
+import org.junit.rules.ExternalResource;
+
+public class FakeAuctionServer extends ExternalResource {
+
+    public FakeAuctionServer(String string) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void startSellingItem() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void hasReceivedJoinRequestFromSniper() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void announceClosed() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
