df809b776321f8e24311fff740b712deeb59b515
Csaba J Santa <santacs@gmail.com>
2015-09-18 07:13:14 +0200

    p91

--- src/main/java/org/tdd/auctionsniper/Main.java
+++ src/main/java/org/tdd/auctionsniper/Main.java
@@ -2,6 +2,9 @@ package org.tdd.auctionsniper;
 
 public class Main {
 
+    public static final String MAIN_WINDOW_NAME = "";
+    public static final String SNIPER_STATUS_NAME = "";
+
     public static void main(String xmppHostname, String sniperId, String sniperPassword,
             String itemId) throws Exception {
         // TODO Auto-generated method stub
