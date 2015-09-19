698366f5f6d7c0f37637592b8695617516b22366
Csaba J Santa <santacs@gmail.com>
2015-09-19 07:53:19 +0200

    p96

--- src/main/java/org/tdd/auctionsniper/Main.java
+++ src/main/java/org/tdd/auctionsniper/Main.java
@@ -1,8 +1,12 @@
 package org.tdd.auctionsniper;
 
+import javax.swing.SwingUtilities;
+
+import org.tdd.auctionsniper.ui.MainWindow;
+
 public class Main {
 
-    public static final String MAIN_WINDOW_NAME = "";
+    public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
     public static final String SNIPER_STATUS_NAME = "";
 
     public static void main(String xmppHostname, String sniperId, String sniperPassword,
@@ -11,4 +15,17 @@ public class Main {
 
     }
 
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+    }
+
+    private MainWindow ui;
+
+    public Main() throws Exception {
+        startUserInterface();
+    }
+
+    private void startUserInterface() throws Exception {
+        SwingUtilities.invokeAndWait(() -> ui = new MainWindow());
+    }
 }
---
+++ src/main/java/org/tdd/auctionsniper/ui/MainWindow.java
@@ -0,0 +1,7 @@
+package org.tdd.auctionsniper.ui;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+}
