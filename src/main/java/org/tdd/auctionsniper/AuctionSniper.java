700a892793b51c2c0e12b178651aee4508e09960
Csaba J Santa <santacs@gmail.com>
2015-09-21 17:11:02 +0200

    p128

--- src/main/java/org/tdd/auctionsniper/AuctionSniper.java
+++ src/main/java/org/tdd/auctionsniper/AuctionSniper.java
@@ -3,9 +3,11 @@ package org.tdd.auctionsniper;
 public class AuctionSniper implements AuctionEventListener {
 
     private final SniperListener sniperListener;
+    private final Auction auction;
 
     public AuctionSniper(Auction auction, SniperListener sniperListener) {
         this.sniperListener = sniperListener;
+        this.auction = auction;
     }
 
     @Override
@@ -15,8 +17,8 @@ public class AuctionSniper implements AuctionEventListener {
 
     @Override
     public void currentPrice(int price, int increment) {
-        // TODO Auto-generated method stub
-
+        auction.bid(price + increment);
+        sniperListener.sniperBidding();
     }
 
 }
