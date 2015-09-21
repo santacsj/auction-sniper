95441a5241991aeefa41232e09f206a32ae92fe2
Csaba J Santa <santacs@gmail.com>
2015-09-21 15:37:08 +0200

    p124

---
+++ src/main/java/org/tdd/auctionsniper/SniperListener.java
@@ -0,0 +1,7 @@
+package org.tdd.auctionsniper;
+
+import java.util.EventListener;
+
+public interface SniperListener extends EventListener {
+    public void sniperLost();
+}
---
+++ src/test/java/org/tdd/auctionsniper/test/AuctionSniperTest.java
@@ -0,0 +1,28 @@
+package org.tdd.auctionsniper.test;
+
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
+import org.junit.Test;
+import org.tdd.auctionsniper.AuctionSniper;
+import org.tdd.auctionsniper.SniperListener;
+
+public class AuctionSniperTest {
+
+    @Rule
+    public final JUnitRuleMockery context = new JUnitRuleMockery();
+
+    private final SniperListener sniperListener = context.mock(SniperListener.class);
+    private final AuctionSniper sniper = new AuctionSniper(sniperListener);
+
+    @Test
+    public void reportsLostWhenAuctionCloses() {
+        context.checking(new Expectations() {
+            {
+                oneOf(sniperListener).sniperLost();
+            }
+        });
+        sniper.auctionClosed();
+    }
+
+}
