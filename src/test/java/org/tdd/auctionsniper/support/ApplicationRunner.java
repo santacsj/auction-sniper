b5381dc15f3d41e8da8f35a99cf267cdce6f403e
Csaba J Santa <santacs@gmail.com>
2015-09-24 16:57:49 +0200

    p140

--- src/test/java/org/tdd/auctionsniper/support/ApplicationRunner.java
+++ src/test/java/org/tdd/auctionsniper/support/ApplicationRunner.java
@@ -40,6 +40,14 @@ public class ApplicationRunner extends ExternalResource {
         driver.showsSniperStatus(MainWindow.STATUS_BIDDING);
     }
 
+    public void hasShownSniperIsWinning() {
+        driver.showsSniperStatus(MainWindow.STATUS_WINNING);
+    }
+
+    public void showsSniperHasWonAuction() {
+        driver.showsSniperStatus(MainWindow.STATUS_WON);
+    }
+
     public void stop() {
         if (driver != null)
             driver.dispose();
