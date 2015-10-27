b793f3ef6db2eca05bad6fe80402c02540694676
Csaba J Santa <santacs@gmail.com>
2015-10-27 07:38:50 +0100

    p223

---
+++ src/main/java/org/tdd/auctionsniper/xmpp/LoggingXMPPFailureReporter.java
@@ -0,0 +1,20 @@
+package org.tdd.auctionsniper.xmpp;
+
+import java.util.logging.Logger;
+
+public class LoggingXMPPFailureReporter implements XMPPFailureReporter {
+
+    private final Logger logger;
+
+    public LoggingXMPPFailureReporter(Logger logger) {
+        this.logger = logger;
+    }
+
+    @Override
+    public void cannotTranslateMessage(String autionId, String failureMessage, Exception exception) {
+        logger.severe(String.format("<%s> Could not translate message \"%s\" because \"%s: %s\"",
+                autionId, failureMessage, exception.getClass().getName(),
+                exception.getLocalizedMessage()));
+    }
+
+}
